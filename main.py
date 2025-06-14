001818d70fcb8012cd251e4e55635f0abb5697ae
Asisg Ranjan Das <24f2007094@ds.study.iitm.ac.in>
2025-06-14 14:45:13 +0530

    Enable GET/OPTIONS methods on /api

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
-from fastapi import FastAPI, UploadFile, File, Form # type: ignore
+from fastapi import FastAPI, UploadFile, File, Form
 from typing import Optional
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-from fastapi.middleware.cors import CORSMiddleware # type: ignore
 
 app.add_middleware(
-  CORSMiddleware,
-  allow_origins=["*"],
-  allow_credentials=True,
-  allow_methods=["*"],
-  allow_headers=["*"],
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods: POST, GET, OPTIONS, etc.
+    allow_headers=["*"],  # Allow all headers
 )
 
-@app.post("/api/")
+@app.api_route("/api", methods=["POST", "GET", "OPTIONS"])
 async def answer_question(
-    question: str = Form(...),
+    question: str = Form(None),
     image: Optional[UploadFile] = File(None)
 ):
+    # For GET requests, just return an info message
+    if question is None:
+        return {"message": "This endpoint supports POST requests with 'question' and optionally 'image'."}
+
     image_info = None
     if image:
         image_info = {
