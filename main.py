4180830f861039e2ef6a23ac4d7c13ef7dd331f3
Asisg Ranjan Das <24f2007094@ds.study.iitm.ac.in>
2025-06-14 11:25:43 +0530

    Fix path + add CORS

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI, UploadFile, File, Form
+from fastapi import FastAPI, UploadFile, File, Form # type: ignore
 from typing import Optional
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware # type: ignore
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["*"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
 
 @app.post("/api/")
 async def answer_question(
